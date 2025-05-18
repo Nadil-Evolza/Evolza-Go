03d3fdd17281fdd1b0388eda2bed9b6d861768a3
NadilGT <rccnadildinsara@gmail.com>
2025-05-18 15:05:31 +0530

    Exercise 3 done

--- Training/exercise01.go
+++ Training/exercise01.go
@@ -4,12 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
-	greetings()
+	//greetings()
+	Rectangle()
 }
 
 func greetings(){
@@ -18,4 +20,26 @@ func greetings(){
 	name, _ := reader.ReadString('\n')
 	name = strings.TrimSpace(name)
 	fmt.Printf("Hello, %s", name)
+}
+
+func Rectangle(){
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter the lenght: ")
+	lengthInput, _ := reader.ReadString('\n')
+	lengthInput = strings.TrimSpace(lengthInput)
+	length, err1 := strconv.ParseFloat(lengthInput, 64)
+
+	fmt.Print("Enter the width: ")
+	widthInput, _ := reader.ReadString('\n')
+	widthInput = strings.TrimSpace(widthInput)
+	width, err2 := strconv.ParseFloat(widthInput, 64)
+
+	if err1 != nil || err2 != nil {
+		fmt.Println("Invalid Inputs. Try Again")
+		return
+	}
+
+	area := length * width
+
+	fmt.Printf("The area of the reactangle is: %v", area)
 }
