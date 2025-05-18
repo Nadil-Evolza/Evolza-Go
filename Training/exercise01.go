d60b1bd53700d9014ac158ccdac922a4cd4dad8c
NadilGT <rccnadildinsara@gmail.com>
2025-05-18 14:39:49 +0530

    Exercise 2 done

--- Training/exercise01.go
+++ Training/exercise01.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	greetings()
+}
+
+func greetings(){
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your name: ")
+	name, _ := reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+	fmt.Printf("Hello, %s", name)
 }
